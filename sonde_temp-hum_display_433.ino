d3c8cb9b67a3001394ea16e7a32d8d4177138ff3
Cedric <ced+git@siroen.fr>
2025-03-19 23:28:42 +0100

    Modification par Jean-Claude du 19/03/25 L'écran fonctionne :-)

--- sonde_temp-hum_display_433.ino
+++ sonde_temp-hum_display_433.ino
@@ -4,59 +4,72 @@
 #include <Adafruit_SHT4x.h>
 #include <RFM69.h>
 #include <SPI.h>
+//#include <LowPower.h>
+#include <RFM69_ATC.h>
+#include "Adafruit_ThinkInk.h"
 
 // Définir les broches pour l'écran eInk
-#define EPD_DC 9
-#define EPD_CS 10
-#define EPD_BUSY -1 // can set to -1 to not use a pin (will wait a fixed delay)
-#define SRAM_CS 8
-#define EPD_RESET 7  // can set to -1 and share with microcontroller Reset!
+#define EPD_DC 10
+#define EPD_CS 9
+#define EPD_BUSY 7 // can set to -1 to not use a pin (will wait a fixed delay)
+#define SRAM_CS 6
+#define EPD_RESET 8  // can set to -1 and share with microcontroller Reset!
 #define EPD_SPI &SPI // primary SPI
 
 // Définir les broches pour le module RFM69HCW
-#define RF69_FREQ 433.92
-#define RF69_CS   6
+#define IS_RFM69HW_HCW
+#define FREQUENCY RF69_433MHZ
+#define RF69_FREQ 433.0
+#define RF69_CS   5
 #define RF69_INT  3
 #define RF69_RST  4
+
 
 Adafruit_SHT4x sht4 = Adafruit_SHT4x();
 RFM69 radio(RF69_CS, RF69_INT, true);
 // Initialiser l'écran eInk
-Adafruit_SSD1680 display(250, 122, EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY, EPD_SPI);
+//Adafruit_IL0373 display(128, 250, EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY);
+ThinkInk_213_Tricolor_RW display(EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY,EPD_SPI);
 
 // Définir les icônes (exemples simples)
 const unsigned char temperatureIcon[] PROGMEM = {
-  0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00,
-  0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00,
-  0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00,
-  0x00, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0x00,
-  0x00, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0x00,
-  0x00, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0x00,
-  0x00, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0x00,
-  0x00, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0x00
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
 };
 
 const unsigned char humidityIcon[] PROGMEM = {
-  0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0x00,
-  0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0x00,
-  0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0x00,
-  0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00,
-  0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00,
-  0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0x00,
-  0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0x00,
-  0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0x00
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
 };
 
 void setup() {
+pinMode(10,OUTPUT);
+pinMode(9,OUTPUT);
+pinMode(7,INPUT);
+pinMode(6,OUTPUT);
+pinMode(8,OUTPUT);
+pinMode(5,OUTPUT);
+pinMode(3,OUTPUT);
+pinMode(4,OUTPUT);
   Serial.begin(115200);
 
   // Initialiser l'écran eInk
-  display.begin();
+  display.begin(THINKINK_TRICOLOR);
   display.clearBuffer();
   // Afficher les icônes et les valeurs
-  display.setTextSize(2);
-  display.setTextColor(EPD_BLACK);
-  
+   
   // Afficher un message de démarrage
   display.setTextSize(2);
   display.setTextColor(EPD_BLACK);
@@ -69,11 +82,11 @@ void setup() {
   display.drawBitmap(10, 10, temperatureIcon, 16, 16, EPD_BLACK);
   display.setCursor(30, 10);
   display.print("Temp: 25.0 C");
-  // // Afficher l'icône d'humidité
+  // Afficher l'icône d'humidité
   display.drawBitmap(10, 40, humidityIcon, 16, 16, EPD_BLACK);
   display.setCursor(30, 40);
   display.print("Humidity: 60%");
-  // // Mettre à jour l'affichage
+  // Mettre à jour l'affichage
   display.display();
 
     // Initialiser le capteur SHT45
@@ -84,14 +97,19 @@ void setup() {
 
    // Initialiser le module RFM69HCW
   radio.initialize(RF69_FREQ, 1, 100);
-  radio.setHighPower();
+  //radio.setHighPower();
+
+  radio.sleep();
 
 }
 
 void loop() {
+  Serial.println("send radio");
+  
   sensors_event_t humidity, temp;
   sht4.getEvent(&humidity, &temp);
-
+  Serial.println(temp.temperature);
+  Serial.println(humidity.relative_humidity);
   // Afficher les données sur l'écran eInk
   display.clearBuffer();
   display.setTextSize(2);
@@ -107,11 +125,17 @@ void loop() {
   display.display();
 
   // Transmettre les données via le module RFM69HCW
-  // Serial.println(temp.temperature);
-  char data[50];
-  sprintf(data, "Temp: %.2f C, Humidity: %.2f %%", temp.temperature, humidity.relative_humidity);
-  radio.send(1, data, strlen(data));
 
+  char data[50];
+sprintf(data, "Temp: %.2f C, Humidity: %.2f %%", temp.temperature, humidity.relative_humidity);
+ // sprintf(data, "Temp: %.2f C, Humidity: %.2f %%", 1848, 1900);
+  Serial.println(data);
+  Serial.println(strlen(data));
+
+//  radio.send(1,"12345678912345678901234567890azertyuiop" , 40);
+  radio.send(1, data, 10);
+Serial.println("data sent");
+//radio.sleep();
   // Attendre 5 minutes
-  delay(5000);
+  delay(50000);
 }
